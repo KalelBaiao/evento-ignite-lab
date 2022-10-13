7083941e16a7db07b12a8c1c9b86aec908a88196
KalelBaiao <106087364+KalelBaiao@users.noreply.github.com>
2022-10-12 21:54:12 -0400

    .

---
+++ react-design-system/src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/global.css'
+
+export function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='font-bold text-5xl text-violet-500'>Hello World</h1>
+      <button className='bg-violet-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-600'>
+        Enviar
+      </button>
+    </>
+  )
+}
